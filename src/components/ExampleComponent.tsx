3140cebbb221ccfd42fff9665f956276525e4731
dcortesnet <dcortes.net@gmail.com>
2021-04-05 23:00:12 -0400

    :sparkles: feat: Editorconfig

--- src/components/ExampleComponent.tsx
+++ src/components/ExampleComponent.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const ExampleComponent = () => {
-    return (
-        <Text>Example Component</Text>
-    )
+  return (
+    <Text>Example Component</Text>
+  )
 }
 
 export default ExampleComponent;
