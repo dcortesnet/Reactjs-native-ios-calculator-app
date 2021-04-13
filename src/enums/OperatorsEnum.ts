301fc0703385db022c26ef661d0ef915e36aa27a
Diego Cortés <dcortes.net@gmail.com>
2021-04-13 16:43:38 -0400

    :sparkles: feat: applyOperator

---
+++ src/enums/OperatorsEnum.ts
@@ -0,0 +1,6 @@
+export enum OperatorsEnum {
+  sum,
+  subtraction,
+  multiplication,
+  division
+}
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,39 +1,28 @@
 import { StyleSheet, Text, View } from "react-native"
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ButtonCal from "../components/ButtonCal";
+import { OperatorsEnum } from "../enums/OperatorsEnum";
 
 const CalculatorScreen = () => {
 
-  const [resultNumber, setResultNumber] = useState('0'); // Número que aparecerá en pantalla
-  const [previusNumber, setPreviusNumber] = useState('0'); // Número de apoyo con historial
-
-  const deleteNumber = () => {
-    setResultNumber('0');
-  }
+  const [resultNumber, setResultNumber] = useState('0');
+  const [previusNumber, setPreviusNumber] = useState('0');
+  const operators = useRef<OperatorsEnum>();
 
   const buildNumber = (value: string) => {
-
-    // Validación inicial remplaza el cero por un número
     if(resultNumber === '0' && value !== '.' && value !== '-' && value !== 'del'){
       setResultNumber(value);
       return;
     }
-
-    // Validación cantidad de digitos
     if(resultNumber.length >= 8 && value !== 'del'){
       return;
     }
-
-    // Validación no aceptar doble punto
     if(resultNumber.includes('.') && value === '.'){
       return;
     }
-
-    // Validación no aceptar doble cero inicial
     if(resultNumber === '0' && value === '0'){
       return;
     }
-
     switch(value){
       case '-': {
         if(resultNumber.startsWith('-')){
@@ -43,21 +32,56 @@ const CalculatorScreen = () => {
         setResultNumber(value + resultNumber);
         return;
       }
-
       case 'del': {
         if(resultNumber.length === 1){
           setResultNumber('0');
           return;
         }
+
         setResultNumber(resultNumber.slice(0, resultNumber.length-1));
         return;
       }
-
       default: {
         setResultNumber(resultNumber + value);
         return;
       }
     }
+  }
+
+  const saveResultNumber = () => {
+    if(resultNumber.endsWith('.')){
+      setPreviusNumber(resultNumber.slice(0, resultNumber.length -1));
+    } else {
+      setPreviusNumber(resultNumber);
+    }
+    setResultNumber('0');
+  }
+
+  const applyOperator = (value: string) => {
+    saveResultNumber();
+    switch (value) {
+      case '+': {
+        operators.current = OperatorsEnum.sum;
+      }
+      case '-': {
+        operators.current = OperatorsEnum.subtraction;
+      }
+      case 'x': {
+        operators.current = OperatorsEnum.multiplication;
+      }
+
+      case '/': {
+        operators.current = OperatorsEnum.division;
+      }
+
+      default:
+        break;
+    }
+  }
+
+  const deleteCal = () => {
+    setResultNumber('0');
+    setPreviusNumber('0');
   }
 
   return (
@@ -67,31 +91,31 @@ const CalculatorScreen = () => {
       <Text style={styles.resultNumberText}>{resultNumber}</Text>
 
       <View style={styles.row}>
-        <ButtonCal text={'C'} color={'black'} backgroundColor={'#9B9B9B'} onPress={deleteNumber}/>
+        <ButtonCal text={'C'} color={'black'} backgroundColor={'#9B9B9B'} onPress={deleteCal}/>
         <ButtonCal text={'+/-'} color={'black'} backgroundColor={'#9B9B9B'} onPress={() => buildNumber('-')}/>
         <ButtonCal text={'del'} color={'black'} backgroundColor={'#9B9B9B'} onPress={() => buildNumber('del')}/>
-        <ButtonCal text={'/'} color={'white'} backgroundColor={'#FF9427'}/>
+        <ButtonCal text={'/'} color={'white'} backgroundColor={'#FF9427'} onPress={() => applyOperator('/')}/>
       </View>
 
       <View style={styles.row}>
         <ButtonCal text={'7'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('7')}/>
         <ButtonCal text={'8'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('8')}/>
         <ButtonCal text={'9'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('9')}/>
-        <ButtonCal text={'x'} color={'white'} backgroundColor={'#FF9427'} />
+        <ButtonCal text={'x'} color={'white'} backgroundColor={'#FF9427'} onPress={() => applyOperator('x')}/>
       </View>
 
       <View style={styles.row}>
         <ButtonCal text={'4'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('4')}/>
         <ButtonCal text={'5'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('5')}/>
         <ButtonCal text={'6'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('6')}/>
-        <ButtonCal text={'-'} color={'white'} backgroundColor={'#FF9427'}/>
+        <ButtonCal text={'-'} color={'white'} backgroundColor={'#FF9427'} onPress={() => applyOperator('-')}/>
       </View>
 
       <View style={styles.row}>
         <ButtonCal text={'1'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('1')}/>
         <ButtonCal text={'2'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('2')}/>
         <ButtonCal text={'3'} color={'white'} backgroundColor={'#2D2D2D'} onPress={() => buildNumber('3')}/>
-        <ButtonCal text={'+'} color={'white'} backgroundColor={'#FF9427'}/>
+        <ButtonCal text={'+'} color={'white'} backgroundColor={'#FF9427'} onPress={() => applyOperator('+')}/>
       </View>
 
       <View style={styles.row}>
